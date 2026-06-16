e39d793e545fdea4369603cdbcdb018cd0883593
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2026-06-16 21:59:24 +0600

    SuccessfulAttemptToSaveData

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -14,9 +15,13 @@ public class Main {
         student.setAge(18);
         /* System.out.println(student); */
         Configuration configuration = new Configuration();
+        configuration.addAnnotatedClass(Student.class);
+        configuration.configure();
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
         session.persist(student);
+        transaction.commit();
         System.out.println(student);
     }
 }
