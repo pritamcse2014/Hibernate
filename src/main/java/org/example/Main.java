924d6875f7e0e9c4e844c5a698310e8dd7cbaf0f
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2026-06-16 21:38:56 +0600

    FailedAttemptToSaveData

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,22 @@
 package org.example;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main(String[] args) {
-
+        Student student = new Student();
+        student.setName("Pritam");
+        student.setRollNo(190322014);
+        student.setAge(18);
+        /* System.out.println(student); */
+        Configuration configuration = new Configuration();
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.persist(student);
+        System.out.println(student);
     }
 }
