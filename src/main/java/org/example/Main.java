9af6654386fd0eb48a7846353ae648dca2bef9b9
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2026-06-20 20:35:09 +0600

    FetchingWithFilterAndSpecificProperties

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -19,8 +19,20 @@ public class Main {
                 .buildSessionFactory();
         Session session = sessionFactory.openSession();
 //        Query query = session.createQuery("from Laptop");
-        Query query = session.createQuery("from Laptop where ram = 64");
-        java.util.List<Laptop> laptops = query.getResultList();
+//        Query query = session.createQuery("from Laptop where ram = 64");
+//        Query query = session.createQuery("from Laptop where brand like 'ASUS'");
+        String brand = "ASUS";
+//        Query query = session.createQuery("from Laptop where brand like ? 1");
+//        Query query = session.createQuery("select model from Laptop where brand like ? 1");
+        Query query = session.createQuery("select brand, model from Laptop where brand like ? 1");
+        query.setParameter(1, brand);
+//        java.util.List<Laptop> laptops = query.getResultList();
+        java.util.List<Object[]> laptops = query.getResultList();
+        for (Object[] laptop : laptops) {
+            System.out.println((String)laptop[0] + " " + (String)laptop[1]);
+        }
+//        java.util.List<String> laptops = query.getResultList();
+//        java.util.List<String[]> laptops = query.getResultList();
 //        Laptop laptop = session.get(Laptop.class, 3);
         System.out.println(laptops);
         session.close();
