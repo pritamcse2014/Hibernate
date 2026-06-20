c90eb834effa1350b82b0e7c966d272603dc22a8
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2026-06-20 21:30:18 +0600

    GetVsFindVsById

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,7 @@
 package org.example;
-
-import jakarta.persistence.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-
-import java.util.Arrays;
-import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -18,23 +12,12 @@ public class Main {
                 .configure()
                 .buildSessionFactory();
         Session session = sessionFactory.openSession();
-//        Query query = session.createQuery("from Laptop");
-//        Query query = session.createQuery("from Laptop where ram = 64");
-//        Query query = session.createQuery("from Laptop where brand like 'ASUS'");
-        String brand = "ASUS";
-//        Query query = session.createQuery("from Laptop where brand like ? 1");
-//        Query query = session.createQuery("select model from Laptop where brand like ? 1");
-        Query query = session.createQuery("select brand, model from Laptop where brand like ? 1");
-        query.setParameter(1, brand);
-//        java.util.List<Laptop> laptops = query.getResultList();
-        java.util.List<Object[]> laptops = query.getResultList();
-        for (Object[] laptop : laptops) {
-            System.out.println((String)laptop[0] + " " + (String)laptop[1]);
-        }
-//        java.util.List<String> laptops = query.getResultList();
-//        java.util.List<String[]> laptops = query.getResultList();
-//        Laptop laptop = session.get(Laptop.class, 3);
-        System.out.println(laptops);
+        Laptop laptop1 = session.get(Laptop.class, 2);
+        System.out.println(laptop1);
+        Laptop laptop2 = session.find(Laptop.class, 2);
+        System.out.println(laptop2);
+        Laptop laptop3 = session.byId(Laptop.class).getReference(2);
+        System.out.println(laptop2);
         session.close();
         sessionFactory.close();
     }
